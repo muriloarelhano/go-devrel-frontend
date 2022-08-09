a91e3f66a28673ea1775428f89f1221c7f48a752
Mariane <mariane.mori@grupocard.com.br>
2022-08-09 13:51:42 -0400

    refactor: info component changes

--- src/components/home/InfoItems/InfoComponent.tsx
+++ src/components/home/InfoItems/InfoComponent.tsx
@@ -1,13 +1,14 @@
 import {  Flex, } from '@chakra-ui/react'
 import InfoItem from './InfoItem'
 
+import {FaBookReader,FaUserFriends, FaUserCheck} from  'react-icons/fa';
+
 const InfoComponent = () => {
   return (
       <Flex alignItems='flex-start' justifyContent='center' flexDir='row'>
-        <InfoItem title='Estude o modelo' subtitle='Temos como missão auxiliar profissionais de developer relations a gerenciar ambientes de desenvolvimento de software, estude com nossos modelos para aprimorar-se'></InfoItem>
-          <InfoItem title='Estude o modelo' subtitle='Temos como missão auxiliar profissionais de developer relations a gerenciar ambientes de desenvolvimento de software, estude com nossos modelos para aprimorar-se'></InfoItem>
-          <InfoItem title='Estude o modelo' subtitle='Temos como missão auxiliar profissionais de developer relations a gerenciar ambientes de desenvolvimento de software, estude com nossos modelos para aprimorar-se'></InfoItem>
-          <InfoItem title='Estude o modelo' subtitle='Temos como missão auxiliar profissionais de developer relations a gerenciar ambientes de desenvolvimento de software, estude com nossos modelos para aprimorar-se'></InfoItem>
+        <InfoItem icon={<FaBookReader size={28} /> } backgroundColor='#4B59F7' title='Estude o modelo' subtitle='Temos como missão auxiliar profissionais de developer relations a gerenciar ambientes de desenvolvimento de software, estude com nossos modelos para aprimorar-se'></InfoItem>
+          <InfoItem icon={<FaUserCheck  size={28}/>} backgroundColor='#805AD5' title='Envie feedbacks' subtitle='Use nossa ferramenta para enviar feedbacks sobre o modelo para que possamos analisar e aprimorar o modelo para ajudar outros e fique por dentro das mudanças'></InfoItem>
+          <InfoItem icon={<FaUserFriends size={28}/>} backgroundColor='#2D3748' title='Ajude a comunidade' subtitle='Enviando suas contribuições você ajuda a comunidade a crescer e desenvolver-se, contribuia para gerar conhecimento para todos'></InfoItem>
 
       </Flex>
   )
--- src/components/home/InfoItems/InfoItem.tsx
+++ src/components/home/InfoItems/InfoItem.tsx
@@ -1,13 +1,12 @@
-import { Box, Flex, Heading,  Text } from '@chakra-ui/react'
-import InfoIcon from '../../../assets/icons/infoIcon.svg'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 
-const InfoItem = ({ title, subtitle }: { title: string, subtitle: string }) => {
+const InfoItem = ({ title, subtitle, backgroundColor, icon }: { title: string, subtitle: string, backgroundColor: string, icon: any }) => {
   return (
-    <Box maxW='316' mr='3' p='0'>
+    <Box maxW='416' mr='3' >
       <Flex alignItems='flex-start' textAlign='left' py='10'>
         <Box>
-          <Box backgroundColor='#4B59F7' w={12} h={12} borderRadius='5' p='3' my='3'boxShadow='0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05);'>
-            <img width='26' height='26'  src={InfoIcon} alt='' />
+          <Box backgroundColor={backgroundColor} color='white' w={12} h={12} borderRadius='5' py='2.5' px='2.5' boxShadow='0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -2px rgba(0, 0, 0, 0.05);'>
+            {icon}
           </Box>
           <Heading py='2' fontSize='18' color='#2D3748' fontWeight='bold'>{title}</Heading>
           <Text fontSize='14' color='#2D3748' fontWeight='regular'>{subtitle}</Text>
