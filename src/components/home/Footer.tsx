1185ae623c2d135c969d18fea2988dfca8318187
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2023-05-09 21:04:45 -0400

    fix: footer position

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,27 +1,45 @@
-import { Container, Stack, Text, Divider, Box } from '@chakra-ui/react'
-import githubIcon from '../../assets/icons/github.svg'
+import { Box, Divider, HStack, Text, VStack } from "@chakra-ui/react";
+import { DateTime } from "luxon";
+import githubIcon from "../../assets/icons/github.svg";
 
 const Footer = () => {
-    const openInNewTab = (url: string) => {
-        window.open(url, '_blank', 'noopener,noreferrer');
-    };
+  const openInNewTab = (url: string) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
-    return (
-        <Container
-            position='absolute' maxW="full" paddingBottom={{ base: '2' }}>
-            <Divider />
-            <Container maxW="container.xl">
-                <Stack py="2" direction="row" justify="space-between" align="center" wrap="wrap">
-                    <Text fontSize='30' fontWeight='bold'>Go DevRel</Text>
-                    <Text fontSize='16'>Direitos reservados GoDevRel 2022©</Text>
-                    <Box cursor='pointer'>
-                        <img width='36' height='36' src={githubIcon} alt='' onClick={() => openInNewTab('https://github.com/muriloarelhano/go-devrel-frontend')} />
-                    </Box>
+  return (
+    <VStack justify={"center"} position={"absolute"} w={"100%"} bottom={0}>
+      <Divider />
+      <HStack
+        py={2}
+        width={"100%"}
+        align="center"
+        maxW="container.xl"
+        justify="space-between"
+      >
+        {" "}
+        <Text fontSize="30" fontWeight="bold">
+          Go DevRel
+        </Text>
+        <Text fontSize="16">
+          Direitos reservados GoDevRel {DateTime.now().year}©
+        </Text>
+        <Box cursor="pointer">
+          <img
+            width="36"
+            height="36"
+            src={githubIcon}
+            alt=""
+            onClick={() =>
+              openInNewTab(
+                "https://github.com/muriloarelhano/go-devrel-frontend"
+              )
+            }
+          />
+        </Box>
+      </HStack>
+    </VStack>
+  );
+};
 
-                </Stack>
-            </Container>
-        </Container>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { withProse } from "@nikolovlazar/chakra-ui-prose";
+import { StepsStyleConfig } from "chakra-ui-steps";
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./pages/App";
-import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
-import { StepsStyleConfig } from "chakra-ui-steps";
-
-import { withProse } from "@nikolovlazar/chakra-ui-prose";
 import { AuthProvider } from "./contexts/AuthContext";
 import { HeaderFormProvider } from "./contexts/HeaderFormContext";
+import { App } from "./pages/App";
+import reportWebVitals from "./reportWebVitals";
 
 const theme = extendTheme(
   {
+    styles: {
+      global: (_props: any) => ({
+        html: {
+          height: "100%",
+          minHeight: "100%",
+        },
+        body: {
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100%",
+        },
+      }),
+    },
     components: {
       Steps: StepsStyleConfig,
     },
