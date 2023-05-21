9c7b5471d12fcf5f0ff4b445d20d40bf732ef239
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2023-05-21 14:17:12 -0400

    fix: unauthorized error

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const http = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_API_HOST ?? "http://localhost:4000",
+  headers: {
+    Authorization: `Bearer ${window.localStorage.getItem("id_token")}`,
+  },
 });
 
 export default http;
